ffa3da2e4f894b3b18ab8f1343bd36769579da10
Phong Luu <huynhpluu@gmail.com>
2021-03-05 13:04:44 +0700

    added: sass

--- src/modules/common/components/nav/index.jsx
+++ src/modules/common/components/nav/index.jsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import {Flex, Spacer} from '@chakra-ui/react'
 import logo from 'assets/logo.png'
+import styles from './styles.module.scss'
 function Nav() {
   return (
     <Flex>
       <img src={logo} alt="Logo" />
       <Spacer />
+      <ul className={styles['list']}>
+        <li>Home</li>
+        <li>About</li>
+      </ul>
       <Spacer />
     </Flex>
   )
