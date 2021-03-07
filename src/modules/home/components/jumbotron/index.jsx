ad15b96d300f1874d5248876ce6916345a17a7a3
Phong Luu <huynhpluu@gmail.com>
2021-03-07 23:20:49 +0700

    fixed: jumbotron css

--- src/modules/home/components/jumbotron/index.jsx
+++ src/modules/home/components/jumbotron/index.jsx
@@ -22,7 +22,7 @@ function Jumbotron() {
         alt=""
       />
       <div className={`${styles['jumbotron-content']} bg-black bg-opacity-75`}>
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+        <div className="flex flex-col justify-between px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <Nav />
           <div className="flex">
             <div className="flex-1">
